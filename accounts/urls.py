f9d63dbda42cab558bae65872ef8677429d13e6e
Asim <asimshrestha608@gmail.com>
2020-12-19 19:33:57 +0545

    feat: created logi page for user.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import register
+from .views import register, login_view
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register', register, name='register'),
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import (UserCreationForm, AuthenticationForm)
+from django.contrib.auth import login
+from django.contrib import messages
 
 
 def register(request):
@@ -7,6 +9,19 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Account created successfully. Please login here.')
+            return redirect('accounts:login')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
