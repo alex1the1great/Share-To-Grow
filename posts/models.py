eb0870d28f846ae68e584544194a7a6953637052
Asim <asimshrestha608@gmail.com>
2020-12-20 09:12:30 +0545

    fix[posts]: updated link field of post model.

--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150)
     slug = models.SlugField()
     body = models.TextField()
-    link = models.URLField(null=True)
+    link = models.URLField(null=True, blank=True)
     pub_date = models.DateTimeField(default=datetime.now())
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
