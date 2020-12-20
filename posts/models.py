060ffb4609cff231fdd161a7de251e40d27b48fc
Asim <asimshrestha608@gmail.com>
2020-12-20 09:05:12 +0545

    chore[posts]: created post model.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from datetime import datetime
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField()
+    body = models.TextField()
+    link = models.URLField(null=True)
+    pub_date = models.DateTimeField(default=datetime.now())
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
