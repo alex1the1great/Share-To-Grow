6656e0e5fc06b3e16db3e01bb32143d299b32fe6
Asim <asimshrestha608@gmail.com>
2020-12-20 09:10:45 +0545

    chore[posts]: register post model to admin page.

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
