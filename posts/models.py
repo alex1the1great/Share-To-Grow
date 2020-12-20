ba55124cec3acbf3e9ba73aa4eb8c17cf381b08b
Asim <asimshrestha608@gmail.com>
2020-12-20 20:55:41 +0545

    refactor[posts]: remove slug field from models file.

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,6 @@ from datetime import datetime
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
-    slug = models.SlugField()
     body = models.TextField()
     link = models.URLField(null=True, blank=True)
     pub_date = models.DateTimeField(default=datetime.now())
